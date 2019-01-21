ecd21b5428db0d73417f3b75f01d2d72feeabe87
Joseph Bozarth <bozarjp@gmail.com>
2019-01-21 14:40:52 -0600

    Cleaned up rest
    Cleaned up the rest class to be a bit cleaner and more consise

--- rest.js
+++ rest.js
@@ -3,17 +3,49 @@ const settings = require('./settings.json');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = settings.rest.tlsRejectUnauthorized == true ? "1" : "0";
 
 exports.post = function(host, port, path, data, headers, callback) {
+	makeReq('POST', host, port, path, headers, callback, function(req) {
+		req.write(data);
+		req.end();
+	});
+};
+
+exports.put = function(host, port, path, data, headers, callback) {
+	makeReq('PUT', host, port, path, data, headers, callback, function(req) {
+		req.write(data);
+		req.end();
+	});
+};
+
+exports.get = function(host, port, path, headers, callback) {
+	makeReq('PUT', host, port, path, data, headers, callback, function(req) {
+		req.end();
+	});
+};
+
+exports.delete = function(host, port, path, headers, callback) {
+	makeReq('DELETE', host, port, path, data, headers, callback, function(req) {
+		req.end();
+	});
+};
+
+function makeReq(method, host, port, path, headers, requestCallback, callback) {
 	const options = {
 		hostname: host,
 		port: port,
 		path: path,
-		method: 'POST',
+		method: method,
 		headers: headers
 	};
 
 	const req = https.request(options, function(res) {
 		if (res.statusCode < 200 || res.statusCode > 299) {
-			callback(null, { error: "Received bad status code", statusCode: res.statusCode });
+			requestCallback(
+				null, 
+				{ 
+					error: "Received bad status code", 
+					statusCode: res.statusCode 
+				}
+			);
 		} else {
 			res.setEncoding('utf8');
 			var body = "";
@@ -33,40 +65,5 @@ exports.post = function(host, port, path, data, headers, callback) {
 		callback(null, { error: "Error on req"});
 	});
 
-	req.write(data);
-	req.end();
-};
-
-exports.get = function(host, port, path, headers, callback) {
-	const options = {
-		hostname: host,
-		port: port,
-		path: path,
-		method: 'GET',
-		headers: headers
-	};
-
-	const req = https.request(options, (res) => {
-		if (res.statusCode < 200 || res.statusCode > 299) {
-			callback(null, { error: "Received bad status code", statusCode: res.statusCode });
-		} else {
-			res.setEncoding('utf8');
-			var body = "";
-
-			res.on('data', (d) => {
-				body += d;
-			});
-
-			res.on("end", () => {
-				callback(body);
-			});
-		}
-	});
-
-	req.on('error', function(e) {
-		console.log(e);
-		callback(null, { error: "Error on req"});
-	});
-
-	req.end();
+	callback(req);
 };
