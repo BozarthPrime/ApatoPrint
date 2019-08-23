159a25e4c6e4f62a4710b3347a0590053050803a
Joseph Bozarth <bozarjp@gmail.com>
2019-08-23 15:14:53 -0500

    Bot breakout (#3)
    * added bot controller
    
    * Broke out slackbot into its own class
    
    It is not very pretty because you can only have one at a time but with the way the slackbot works it is unfortunately necessary. I need to add a todo to just rip out this library and write my own so I do not need this state object.

---
+++ bot-controllers/slack-controller.js
@@ -0,0 +1,70 @@
+const SlackBot = require('slackbots');
+const RestHandler = require('../rest-handler');
+const slackRest = new RestHandler('https://slack.com', "", true);
+const log = require("../logger");
+const state = {};
+
+class slackController {
+	constructor(settings, messageHandler) {
+		state.bot = new SlackBot({ token: settings.token });
+		state.settings = settings;
+		state.messageHandler = messageHandler;
+		console.log(state);
+
+        state.bot.on("start", function() {
+			log.info("Connection to Slack established. Posting conformation message to command channel.");
+			
+			state.bot.postMessageToGroup(
+				state.settings.commandChannelName,
+				state.settings.connectionMessage != undefined ? 
+				state.settings.connectionMessage : "I am now online!",
+				{ icon_emoji: state.settings.iconEmoji }
+			).fail(function(data) {
+				log.error("There was an error posting connection message. error=" + data.error);
+			});
+		});
+
+        state.bot.on("message", function(data){
+			if (data != undefined && data != null) {
+				if (data.type == "message" && data.channel == state.settings.commandChannelId) {
+					if (data.subtype != 'bot_message' && data.text != undefined && data.text != null) {
+						state.messageHandler(data.text);
+					}
+				}
+			}
+        });
+	}
+	
+	postToCommandChannel(message) {
+		state.bot.postMessageToGroup(
+			state.settings.commandChannelName,
+			message,
+			{ icon_emoji: state.settings.iconEmoji }
+		);
+	}
+
+	uploadFile(imageName, imagePath) {
+		slackRest.postForm( 
+			'/api/files.upload', 
+			{
+				token: state.settings.token,
+				title: imageName,
+				filename: imageName,
+				filetype: "auto",
+				channels: state.settings.commandChannelId,
+				file: fs.createReadStream(imagePath),
+			}, 
+			function (err, response) {
+				if (err != null) {
+					log.error("Error in uploadStatusPicture:" + JSON.stringify(err));
+					
+					postToCommandChannel(
+						"Slack file upload failed: \n>>>" + err.message
+					);
+				}
+			}
+		);
+	}
+}
+
+module.exports = slackController;
